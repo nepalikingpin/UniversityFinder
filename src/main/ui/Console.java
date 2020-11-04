5a9ca4d4d524e8e90cfac1fa607584d38f1c7318
dsingal2 <dhairyasingal08@gmail.com>
2020-11-04 11:36:43 -0800

    added basic Gui front end

--- src/main/ui/Console.java
+++ src/main/ui/Console.java
@@ -6,6 +6,10 @@ import model.*;
 import persistence.JsonReader;
 import persistence.JsonWriter;
 
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -13,8 +17,8 @@ import java.util.HashSet;
 import java.util.Scanner;
 import java.util.Set;
 
-public class Console {
-    int start;
+public class Console implements ActionListener {
+
     String interests = "";
     String major = "";
     String location = "";
@@ -26,26 +30,157 @@ public class Console {
     DataChoices dc = new DataChoices("","","","");
     AddToData atd = new AddToData(dc, dataList);
     SuggestUniversity suggest = new SuggestUniversity(userList, dataList);
+    String guiDisplay = "";
 
     Scanner in = new Scanner(System.in);
     private static final String JSON_STORE = "./data/universitySuggestions.json";
     private JsonWriter jsonWriter;
     private JsonReader jsonReader;
 
+    JFrame frame;
+    JPanel initialPanel;
+    JPanel secondPanel;
+    JPanel thirdPanel;
+    JButton b1;
+    JButton b2;
+    JButton b3;
+    JButton b4;
+    JLabel label;
+
     public Console() throws IllegalStateException {
         jsonWriter = new JsonWriter(JSON_STORE);
         jsonReader = new JsonReader(JSON_STORE);
 
-        System.out.println("Welcome to University Finder! Find the best suited universities for you based"
-                + " on your interests, major and location!");
-        System.out.println("Enter 0 to quit" + "| Enter 1 to display a list of universities in the database, and start|"
-                + "Enter any other number to directly start | Enter 3 to load previously recommended universities");
-        start = in.nextInt();
-        loadSuggestions(start);
-        start();
+        initializeGui();
+
     }
 
-    void start() {
+    void initializeGui() {
+        frame = new JFrame();
+
+        sceneOne();
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setTitle("University Finder");
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    void sceneOne() {
+        initialPanel = new JPanel();
+        initialPanel.setBorder(BorderFactory.createEmptyBorder(30,30,10,50));
+        initialPanel.setLayout(new GridLayout(0,1));
+
+        label = new JLabel("<html>" + "Welcome to University Finder! Find the best suited universities for you "
+                + "based on your interests, major and location!" + "</html>");
+        initialPanel.add(label);
+
+        label = new JLabel("\n");
+        initialPanel.add(label);
+
+        b1 = new JButton("Display List of Universities (and start)");
+        b1.addActionListener(this);
+        initialPanel.add(b1);
+
+        b2 = new JButton("Start the Program");
+        b2.addActionListener(this);
+        initialPanel.add(b2);
+
+        b3 = new JButton("Load previous recommendations");
+        b3.addActionListener(this);
+        initialPanel.add(b3);
+
+        frame.add(initialPanel, BorderLayout.CENTER);
+
+
+    }
+
+    void sceneTwo() {
+        frame.remove(initialPanel);
+
+        secondPanel = new JPanel();
+        secondPanel.setBorder(BorderFactory.createEmptyBorder(30,30,10,50));
+        secondPanel.setLayout(new GridLayout(0,1));
+
+        sceneTwoSelectInterests();
+        sceneTwoSelectMajor();
+        sceneTwoSelectLocation();
+
+        frame.add(secondPanel, BorderLayout.CENTER);
+        frame.validate();
+
+    }
+
+    void sceneTwoSelectInterests() {
+        label = new JLabel("<html>" + "Please select your interests" + "</html>");
+        secondPanel.add(label);
+
+        b1 = new JButton("E sports");
+        secondPanel.add(b1);
+
+        b2 = new JButton("Robotics");
+        secondPanel.add(b2);
+
+        b3 = new JButton("Soccer");
+        secondPanel.add(b3);
+    }
+
+    void sceneTwoSelectMajor() {
+        label = new JLabel("<html>" + "Please select your intended Major" + "</html>");
+        secondPanel.add(label);
+
+        b1 = new JButton("CompSci");
+        secondPanel.add(b1);
+
+        b2 = new JButton("Math");
+        secondPanel.add(b2);
+
+        b3 = new JButton("Commerce");
+        secondPanel.add(b3);
+    }
+
+    void sceneTwoSelectLocation() {
+        label = new JLabel("<html>" + "Please select your intended location" + "</html>");
+        secondPanel.add(label);
+
+        b1 = new JButton("CompSci");
+        secondPanel.add(b1);
+
+        b2 = new JButton("Math");
+        secondPanel.add(b2);
+
+        b3 = new JButton("Commerce");
+        secondPanel.add(b3);
+    }
+
+    void sceneThree() {
+        frame.remove(secondPanel);
+
+        thirdPanel = new JPanel();
+        thirdPanel.setBorder(BorderFactory.createEmptyBorder(30,30,10,50));
+        thirdPanel.setLayout(new GridLayout(0,1));
+
+        sceneThreeOptions();
+
+        frame.add(thirdPanel, BorderLayout.CENTER);
+        frame.validate();
+    }
+
+    void sceneThreeOptions() {
+
+        b1 = new JButton("Save Suggested Universities");
+        thirdPanel.add(b1);
+
+        b2 = new JButton("Add More Choices");
+        thirdPanel.add(b2);
+
+        b3 = new JButton("Delete Current Choices");
+        thirdPanel.add(b3);
+
+        b4 = new JButton("Quit and See Recommendations");
+    }
+
+    void start(int start) {
         while (start != 0) {
             displayDatabase(start);
             System.out.println("Please select interests" + "\n" + "1: E sports" + "\n" + "2: Robotics"
@@ -178,16 +313,28 @@ public class Console {
     // Code partly taken from JsonSerializationDemo
     // MODIFIES: this
     // EFFECTS: loads workroom from file
-    void loadSuggestions(int start) {
-        if (start == 3) {
-            try {
-                suggest = jsonReader.read();
-                System.out.println(suggest.getSuggestionList() + "\n");
-                System.out.println("Loaded from " + JSON_STORE);
-            } catch (IOException e) {
-                System.out.println("Unable to read from file: " + JSON_STORE);
-            }
-            this.start = 0;
+    void loadSuggestions() {
+        try {
+            suggest = jsonReader.read();
+            guiDisplay += suggest.getSuggestionList() + "\n";
+            System.out.println(guiDisplay);
+            System.out.println("Loaded from " + JSON_STORE);
+        } catch (IOException e) {
+            System.out.println("Unable to read from file: " + JSON_STORE);
+        }
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        Object obj = e.getSource();
+
+        if (obj == b1) {
+            displayDatabase(1);
+            sceneTwo();
+        } else if (obj == b2) {
+            start(7);
+        } else if (obj == b3) {
+            loadSuggestions();
         }
     }
 }
